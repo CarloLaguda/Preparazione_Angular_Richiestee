61b5f4ac397b30a0cd42c1bbe6c7834c1f416bfa
CarloLaguda <carlo.laguda07@gmail.com>
2025-05-12 08:57:21 +0000

    punti 3 e 4

--- Prenotazioni/src/app/app.component.ts
+++ Prenotazioni/src/app/app.component.ts
@@ -1,12 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { Prenotation } from './models/Prenotation.model';
+import { CommonModule } from '@angular/common';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Prenotazioni';
+  //Oggetti classe prenottation
+  prenotazioni : Prenotation[] = []
+  oPrenotazioni! : Observable<Prenotation[]>;
+  //Oggetti per chiamate http
+  http: HttpClient;
+  data!: Object;
+  loading!: boolean;
+  o! :Observable<Object>;
+
+  //Inizializzo l'oggetto HTTP
+  constructor(http: HttpClient)
+  {
+    this.http = http
+  }
+  
+
+  makeRequest(): void{
+    this.loading = true
+    this.oPrenotazioni = this.http.get<Prenotation[]>("https://my-json-server.typicode.com/malizia-g/verificaPrenotazioni/prenotazioni")
+    this.oPrenotazioni.subscribe(this.getData);
+  }
+
+  getData = (data : Prenotation[]) => {
+    this.prenotazioni = data;
+    console.log(this.prenotazioni)
+  }
+  
+  ngOnInit() {
+    this.makeRequest()
+  }
 }
--- Prenotazioni/src/app/models/Prenotation.model.ts
+++ Prenotazioni/src/app/models/Prenotation.model.ts
@@ -1,4 +1,4 @@
-export class prenotation
+export class Prenotation
 {
     nome: string
     cognome: string
