cf86270ea8c27d7c500430606d766323789927cf
Carlo <159021623+CarloLaguda@users.noreply.github.com>
2025-05-13 10:56:03 +0000

    punto 8

---
+++ Prenotazioni/src/app/dettagli-prenotazione/dettagli-prenotazione.component.ts
@@ -0,0 +1,12 @@
+import { Component, Input } from '@angular/core';
+import { Prenotation } from '../models/Prenotation.model';
+
+@Component({
+  selector: 'app-dettagli-prenotazione',
+  imports: [],
+  templateUrl: './dettagli-prenotazione.component.html',
+  styleUrl: './dettagli-prenotazione.component.css'
+})
+export class DettagliPrenotazioneComponent {
+  @Input() singoli_dettagli: Prenotation = new Prenotation("", "", "", 0, "", "", "")
+}
--- Prenotazioni/src/app/lista-prenotazioni/lista-prenotazioni.component.ts
+++ Prenotazioni/src/app/lista-prenotazioni/lista-prenotazioni.component.ts
@@ -1,12 +1,19 @@
 import { Component, Input } from '@angular/core';
 import { Prenotation } from '../models/Prenotation.model';
+import { DettagliPrenotazioneComponent } from '../dettagli-prenotazione/dettagli-prenotazione.component';
 
 @Component({
   selector: 'app-lista-prenotazioni',
-  imports: [],
+  imports: [DettagliPrenotazioneComponent],
   templateUrl: './lista-prenotazioni.component.html',
   styleUrl: './lista-prenotazioni.component.css'
 })
 export class ListaPrenotazioniComponent {
   @Input() prenotazione: Prenotation = new Prenotation("", "", "", 0, "", "", "")
+  dettagli: boolean = false
+  visualizza_dettagli(): boolean
+  {
+    this.dettagli = !this.dettagli
+    return false
+  }
 }
